02da0f20b7679bdc34986e4b7e3c8a0f438ab972
Ineke Nguyen <71349960+mikenkie@users.noreply.github.com>
2024-11-25 21:52:11 -0500

    file writers

---
+++ include/fileWriter.h
@@ -0,0 +1,33 @@
+
+#ifndef FILEWRITER_H_
+#define FILEWRITER_H_
+
+#include <iostream>
+
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include "cTimer.h"
+#include <sys/dispatch.h>
+#include <sys/siginfo.h>
+#include <sys/trace.h>
+
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <cstring>
+
+class fileWriter {
+public:
+	fileWriter();
+	virtual ~fileWriter();
+
+	void writeLow();
+	void writeMed();
+	void writeHigh();
+	void writeOver();
+};
+
+#endif /* FILEWRITER_H_ */
